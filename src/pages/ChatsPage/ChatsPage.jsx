f23b37d73febe0d8a5658cc244194d729a2052fe
Mario Quintero <mario.quintero@correo.unimet.edu.ve>
2023-02-26 13:22:48 -0400

    chat1 basico

--- src/pages/ChatsPage/ChatsPage.jsx
+++ src/pages/ChatsPage/ChatsPage.jsx
@@ -1,5 +1,42 @@
+import { useEffect, useState } from "react";
+import {addDoc, collection, onSnapshot, query, serverTimestamp, where} from "firebase/firestore";
+import {auth, db} from "../../firebase/config"
 import { RiChat1Line,RiMore2Fill,RiArchiveLine,RiSearchEyeLine,RiCheckDoubleFill,RiCameraLine,RiLinkM,RiEmotionLine,RiSendPlane2Fill} from "react-icons/ri";
+
+
 export function ChatsPage() {
+
+    const [newMessage, setNewMessage] = useState("");
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        const queryMessages = query(messagesRef, where("room", "==", "pruebas"))
+        onSnapshot(queryMessages, (snapshot) => {
+            let messages = [];
+            snapshot.forEach((doc) => {
+                messages.push({...doc.data(), id: doc.id});
+            });
+        setMessages(messages);
+
+        });
+    }, []);
+
+    const messagesRef = collection(db, "messages");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (newMessage === "") return;
+
+        await addDoc(messagesRef, {
+            text: newMessage,
+            createdAt: serverTimestamp(),
+            
+            room: "pruebas"
+        });
+
+        setNewMessage("");
+    };
+
     return (
         <div className="min-h-screen grid grid-cols-1 xl:grid-cols-4 bg-[#FFFFFF] text-white-500">
             {/*Contactos*/}
@@ -150,35 +187,50 @@ export function ChatsPage() {
                 </header>
                 {/*Mensajes*/}
                 <main className="h-[84vh] overflow-y-scroll p-4">
-                        {/*Mensaje 1*/}
+                    {/* mensajes de prueba */}
+                    <div>
+                        {/* Mensaje 1
                         <div className="mb-3 flex">
                             <p className="bg-[#D5D6DC] max-w-[80%] xl:max-w-2xl py-1 px-4 rounded-tr-xl rounded-br-xl rounded-bl-xl">
                                 Hola
                             </p>
                         </div>
-                        {/*Mensaje 2*/}
+                        {Mensaje 2}
                         <div className="mb-3 flex">
                             <p className="bg-[#D5D6DC] max-w-[80%] xl:max-w-2xl py-1 px-4 rounded-tr-xl rounded-br-xl rounded-bl-xl">
                                 Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
                             </p>
                         </div>
-                        {/*Mensaje 3*/}
+                        {Mensaje 3}
                         <div className="mb-3 flex justify-end">
                             <p className="bg-[#ab90b9] max-w-[80%] xl:max-w-2xl py-1 px-4 rounded-tl-xl rounded-bl-xl rounded-br-xl">
                                 Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
                             </p>
+                        </div> */}
+                    </div>
+                    {/* mensajes reales */}
+                    {messages.map((message) => 
+                        <div className="mb-3 flex">
+                                <p className="bg-[#D5D6DC] max-w-[80%] xl:max-w-2xl py-1 px-4 rounded-tr-xl rounded-br-xl rounded-bl-xl">
+                                    {message.text}
+                                </p>
                         </div>
+                    )}
+
                 </main>
                 {/*Enviar mensajes*/}
                 <div className="h-[8vh] text-white-500 flex items-center bg-[#ab90b9]">   
-                    <form className="w-[60%] xl:w-10/12">
+                    <form className="w-[60%] xl:w-10/12 flex" onSubmit={handleSubmit}>
                         <input type="text" className="bg-[#D5D6DC] w-full py-2 px-6 rounded-full outline-none text-white-500"
-                        placeholder="Escriba un mensaje"
+                            placeholder="Escriba un mensaje"
+                            onChange={(e)=> setNewMessage(e.target.value)}
+                            value={newMessage}
                         />
+                        <button type="submit">
+                            <RiSendPlane2Fill className="hover:cursor-pointer"/>
+                        </button>
                     </form>
-                    <div className="w-[40%] xl:w-1/12 flex justify-center text-2xl">
-                        <RiSendPlane2Fill className="hover:cursor-pointer"/>
-                    </div>
+                         
                 </div>
             </div>
         </div>
