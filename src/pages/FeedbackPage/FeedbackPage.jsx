4e074a3674cd6b1c30bc4b887bd6ad19eb063a22
Emilgon <g.emilio@correo.unimet.edu.ve>
2023-03-25 18:36:59 -0400

    feedback penúltimo

--- src/pages/FeedbackPage/FeedbackPage.jsx
+++ src/pages/FeedbackPage/FeedbackPage.jsx
@@ -1,10 +1,37 @@
 
 //import React, { useState } from "react";
 import "./FeedbackPage.css";
-import {useState} from "react";
+import {useState,useEffect} from "react";
+import { getDoctorProfile } from "../../firebase/doctors-service";
 import {AiFillStar,AiOutlineStar} from "react-icons/ai";
+import { getUserProfile } from "../../firebase/users-service";
+import { useUserContext } from "../../contexts/UserContext";
+import { updateDoctorFeedback } from "../../firebase/feedback-service";
 //FeedBackPage se encarga de mostrar el 5 rating stars y la caja de comentarios al doctor
 export function FeedbackPage() {
+    const {user} = useUserContext()
+    const [doctor, setDoctor] = useState({displayName:"",specialty:""});
+    const [feedback,setFeedback]=useState({displayName:user.displayName,hoverStar:0,comentario:""})
+    const handleOnChange = (event) => {
+        const {name, value} = event.target
+        setFeedback({
+            ...feedback,
+            [name]: value
+        })
+    }
+    useEffect(() => {
+        const loadDoctor = async () => {
+          await getDoctorProfile(user.lastDoctor).then((result) =>{
+              setDoctor(result);
+          })
+        }
+        loadDoctor();
+  
+        return () => {
+        
+        };
+  
+      }, []);
     const [number,setNumber]=useState(0);
     const [hoverStar,setHoverStar]=useState(undefined);
     const handleText=()=>{
@@ -25,6 +52,14 @@ export function FeedbackPage() {
                 return 'Califique su experiencia con este doctor'    
         }
     };
+    const changeStars=(index)=>{
+        setNumber(index+1)
+        setFeedback({
+            ...feedback,
+            hoverStar: index+1
+        })
+
+    }
 
     const handlePlaceHolder=()=>{
         switch(number||hoverStar){
@@ -39,47 +74,58 @@ export function FeedbackPage() {
             case 5:
                 return '¿Por qué te gustó este doctor?';
             default:
-                return 'Escriba sus comentarios';
-                        
+                return 'Escriba sus comentarios';          
         }
     };
+    const onSubmit = async (event) => {
+        event.preventDefault()
+        await updateDoctorFeedback(doctor,feedback).then(() => {
+            console.log("éxito máximo") //TODO navigate to consults
+         })
+    }
     return (
         <div className="App">
             <div className="popup">
                 <div className="content">
                     <div className="doctor">
                         <img className="doctorPhoto" 
-                            style={{width:155,height:155,objectFit:"cover",borderRadius:45}} 
+                            style={{width:200,height:200,objectFit:"cover",borderRadius:45}} 
                             src="https://pbs.twimg.com/profile_images/1530563277/picoro_400x400.jpg" 
                             alt="name" 
                         />
-                        <h1>Juanito De La Calzada</h1>
-                        <h3>Su última consulta con este doctor fue 12/12/2023</h3>
+                        <div className="caracter">
+                            <h1>{doctor.displayName}</h1>
+                            <h3>Especialidad: {doctor.specialty}</h3>
+                        </div>
+                        
                     </div>
-                    <div className="stars">
-                        <h1>{handleText()}</h1>
-                        {Array(5)
-                            .fill()
-                            .map((_, index)=>
-                                number >= index + 1 || hoverStar >= index + 1 ? (
-                                    <AiFillStar 
-                                        onMouseOver={() => !number && setHoverStar(index + 1)}
-                                        onMouseLeave={() => setHoverStar(undefined)}
-                                        style={{color:'orange'}} 
-                                        onClick={() => setNumber(index + 1)}
-                                    />
-                                ):(
-                                    <AiOutlineStar 
-                                        onMouseOver={() => !number && setHoverStar(index + 1)}
-                                        onMouseLeave={() => setHoverStar(undefined)}
-                                        style={{ color: "orange" }}
-                                        onClick={() => setNumber(index + 1)}
-                                    />
-                                )       
-                            )}
-                    </div>
-                    <textarea placeholder={handlePlaceHolder()}></textarea>
-                    <button className={` ${!number && "disabled"} `}>Enviar feedback</button>
+                    <form onSubmit={onSubmit}>
+                        <div className="stars">
+                            <h1>{handleText()}</h1>
+                            {Array(5)
+                                .fill()
+                                .map((_, index)=>
+                                    number >= index + 1 || hoverStar >= index + 1 ? (
+                                        <AiFillStar 
+                                            onMouseOver={() => !number && setHoverStar(index + 1)}
+                                            onMouseLeave={() => setHoverStar(undefined)}
+                                            style={{color:'orange'}} 
+                                            onClick={() => changeStars(index)}
+                                        />
+                                    ):(
+                                        <AiOutlineStar 
+                                            onMouseOver={() => !number && setHoverStar(index + 1)}
+                                            onMouseLeave={() => setHoverStar(undefined)}
+                                            style={{ color: "orange" }}
+                                            onClick={() => changeStars(index)}
+                                        />
+                                    )       
+                                )}
+                        </div>
+                        <textarea onChange={handleOnChange} name="comentario" placeholder={handlePlaceHolder}></textarea>
+                        <button type="submit" className={` ${!number && "disabled"} `}>Enviar feedback</button>
+                    </form>
+                    
                 </div>
             </div>
         </div>
