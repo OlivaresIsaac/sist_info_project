666bdc598990ee37c560ca504b60da4ed841e933
lddg0502 <ldomingos@correo.unimet.edu.ve>
2023-03-27 15:32:36 -0400

    arreglos ultimo dia

--- src/pages/ConsultsPage/ConsultsPage.jsx
+++ src/pages/ConsultsPage/ConsultsPage.jsx
@@ -1,7 +1,48 @@
-export function ConsultsPage() {
+import './LandingPage.css'
+import psicologo from '../../assets/psicologo.png'
+import team from '../../assets/team2.png'
+import AbstractCard from "../../components/AbstractCard/AbstractCard";
+import { useState, useEffect } from "react"
+import { getDoctors } from "../../firebase/doctors-service";
+
+
+export function ConsultsPage(cita) {
+    const [info, setInfo] = useState([])
+    const [isDoc, setisDoc] = useState(false)
+    
+        
+    useEffect(() => {
+        
+    
+    const loadDoctors = async () => {
+        await getDoctors().then((result) => {
+            setInfo(result)
+            // setDoctors(result)
+            // console.log(result)
+        })
+           
+    }
+    
+        loadCita()
+    
+        return () => {
+          
+        };
+    }, []);   
 
     return (<div>
-        <h1>Soy Consultas</h1>
+        <div className='abstractCardContainer'>
+                {
+                    info.map((cardInfo, key) => {
+                       return(
+                        <div className='abstractCard'> 
+                       <AbstractCard Info={cardInfo} isDoc={isDoc} key={key}/>
+                       </div> 
+                       )
+                    })
+                }
+               
+            </div>
     </div>
     )
 }
