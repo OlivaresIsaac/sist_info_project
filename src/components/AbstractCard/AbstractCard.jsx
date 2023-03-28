9a82527a25b6ce391847ae2192266b474ac0fe1b
lddg0502 <ldomingos@correo.unimet.edu.ve>
2023-03-27 23:30:57 -0400

    cancelar cita

--- src/components/AbstractCard/AbstractCard.jsx
+++ src/components/AbstractCard/AbstractCard.jsx
@@ -2,14 +2,21 @@ import styles from "./AbstractCard.module.css";
 import DocCard from "../DocCard/DocCard";
 import CitaCard from "../CitaCard/CitaCard";
 import CheckoutDialog from '../../components/CheckoutDialog/Dialog'
+import {updateUserConsulta} from '../../firebase/consult-service'
+
 
 // Componente que crea el molde para las cartas de los doctores, implementa el patrón Factoria Abstracta.
 
 export default function Card({isDoc, Info}) {
+
+
+
+
     return (
         
-        <div className={styles.cardContainer}>
+        <div className={styles.cardContainer} >
             <div className={styles.cardContainerImg}>
+                {/* <img className={styles.cardImg} src={Info.imagen} alt="imagen"/> */}
             </div>
             <div className={styles.CardInfo}>
                 
@@ -34,7 +41,7 @@ export default function Card({isDoc, Info}) {
                     </>
                     )}
                     {!isDoc && (
-                        <button className={styles.CardButton}>Cancelar Cita</button>
+                        <button onClick={() => updateUserConsulta(Info)} className={styles.CardButton}>Cancelar Cita</button>
                     )}
             </div>
         </div>
